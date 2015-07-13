327d6bfe26c82fba237298bfb00c58ae8cf93b06
FlorencioConcha <nonimof@gmail.com>
2015-07-13 17:50:09 +0200

    random version 3

--- random.js
+++ random.js
@@ -1,9 +1,10 @@
  primero despues el segundo
 
 var numero = Math.random();
-if (numero <0 0.5){
-  console.log ('\n' + numero + 'Menor que 0,5 \n');
+var str = ?Mayor que 0,5;
+if (numero <= 0.5){
+  str = 'Menor que 0,5 ');
 }
 else {
-  console.log ('\n' + numero + 'Mayor que 0,5 \n');
+  console.log ('\n' + numero + str + ' \n');
 }
